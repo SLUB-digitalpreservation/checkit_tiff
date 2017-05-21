104e0ba0c4c2645f30deaa55d7df429ac40823cc
andreas.romeyke@slub-dresden.de <romeyke@slub-dresden.de>
2017-05-21 20:12:09 +0200

    - reverted line-code, because is is not useable with left recursive parsing - added get_parser_function_name() used for human readable exe stack

--- src/messages/msg_parser.c
+++ src/messages/msg_parser.c
@@ -8,6 +8,35 @@
 #include "config_parser.h"
 #include <assert.h>
 
+const char * get_parser_function_name( function_t f ) {
+  switch (f) {
+    case fc_true: return "fc_true"; break;
+    case fc_false: return "fc_false"; break;
+    case fc_tag_has_some_of_these_values: return "fc_tag_has_some_of_these_values"; break;
+    case fc_tag_has_valuelist: return "fc_tag_has_valuelist"; break;
+    case fc_tag_has_value_in_range: return "fc_tag_has_value_in_range"; break;
+    case fc_tag_has_value: return "fc_tag_has_value"; break;
+    case fc_tag_has_value_quiet: return "fc_tag_has_value_quiet"; break;
+    case fc_tag: return "fc_tag"; break;
+    case fc_tag_quiet: return "fc_tag_quiet"; break;
+    case fc_notag: return "fc_notag"; break;
+    case fc_tag_has_valid_type: return "fc_tag_has_valid_type"; break;
+    case fc_datetime: return "fc_datetime"; break;
+    case fc_icc: return "fc_icc"; break;
+    case fc_has_only_one_ifd: return "fc_has_only_one_ifd"; break;
+    case fc_tagorder: return "fc_tagorder"; break;
+    case fc_tag_has_valid_asciivalue: return "fc_tag_has_valid_asciivalue"; break;
+    case fc_tag_has_value_matching_regex: return "fc_tag_has_value_matching_regex"; break;
+    case fc_all_offsets_are_word_aligned: return "fc_all_offsets_are_word_aligned"; break;
+    case fc_all_offsets_are_used_once_only: return "fc_all_offsets_are_used_once_only"; break;
+    case fc_all_IFDs_are_word_aligned: return "fc_all_IFDs_are_word_aligned"; break;
+    case fc_internal_logic_combine_open: return "fc_internal_logic_combine_open"; break;
+    case fc_internal_logic_combine_close: return "fc_internal_logic_combine_close"; break;
+    case fc_dummy: return "fc_dummy"; break;
+  }
+  return "missed function description, should not occur";
+}
+
 const char * get_parser_function_description( function_t f ) {
   switch (f) {
     case fc_true: return "true"; break;
