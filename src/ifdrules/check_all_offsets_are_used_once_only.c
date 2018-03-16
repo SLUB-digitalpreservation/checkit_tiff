4c84438d446fc0b9196375afdc85112fbbbd7d4c
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2018-03-16 17:05:57 +0100

    - fixed bug found by ubsan

--- src/ifdrules/check_all_offsets_are_used_once_only.c
+++ src/ifdrules/check_all_offsets_are_used_once_only.c
@@ -15,37 +15,39 @@
 ret_t check_all_offsets_are_used_once_only(ctiff_t * ctif) {
   GET_EMPTY_RET(ret)
   tifp_check( ctif);
-  int count = get_ifd0_count( ctif);
-  int tagidx;
-  uint32 offsets[ count ];
-  uint32 tags[ count ];
-  int count_of_offsets = 0;
-  int i;
+  uint16 count = get_ifd0_count( ctif);
+  if (count > 0) {
+    uint16 tagidx;
+    uint32 offsets[ count ];
+    uint32 tags[ count ];
+    int count_of_offsets = 0;
+    int i;
 
-  for (tagidx = 0; tagidx< count; tagidx++) {
-    offsets[ tagidx ] = 0;
-    tags[ tagidx ] = 0;
-  }
-  for (tagidx = 0; tagidx< count; tagidx++) {
-    ifd_entry_t ifd_entry = TIFFGetRawTagIFDListEntry( ctif, tagidx );
-    if (ifd_entry.value_or_offset==is_offset) {
-      uint32 offset = ifd_entry.data32offset;
-      uint32 tag = TIFFGetRawTagListEntry( ctif, tagidx);
-      for (i=0; i< count_of_offsets; i++) {
-        if (offsets[ i ] == offset) {
-          // FIXME: tif_fails?
-          char array[TIFFAILSTRLEN];
-          snprintf(array, sizeof(array), "offset of tag %u (%s) points to %08x, which address is used previously by tag %u (%s)", tag, TIFFTagName(tag), offset, tags[i], TIFFTagName(tags[i]) );
-          ret.returncode = ifderror_offset_used_twice;
-          ret = set_value_found_ret(&ret, array);
-          return ret;
+    for (tagidx = 0; tagidx< count; tagidx++) {
+      offsets[ tagidx ] = 0;
+      tags[ tagidx ] = 0;
+    }
+    for (tagidx = 0; tagidx< count; tagidx++) {
+      ifd_entry_t ifd_entry = TIFFGetRawTagIFDListEntry( ctif, tagidx );
+      if (ifd_entry.value_or_offset==is_offset) {
+        uint32 offset = ifd_entry.data32offset;
+        uint32 tag = TIFFGetRawTagListEntry( ctif, tagidx);
+        for (i=0; i< count_of_offsets; i++) {
+          if (offsets[ i ] == offset) {
+            // FIXME: tif_fails?
+            char array[TIFFAILSTRLEN];
+            snprintf(array, sizeof(array), "offset of tag %u (%s) points to %08x, which address is used previously by tag %u (%s)", tag, TIFFTagName(tag), offset, tags[i], TIFFTagName(tags[i]) );
+            ret.returncode = ifderror_offset_used_twice;
+            ret = set_value_found_ret(&ret, array);
+            return ret;
+          }
         }
+        offsets[ ++count_of_offsets ] = offset;
+        tags[ count_of_offsets ] = tag;
       }
-      offsets[ ++count_of_offsets ] = offset;
-      tags[ count_of_offsets ] = tag;
     }
   }
-  ret.returncode=is_valid;
+  ret.returncode=is_valid; /*  valid also if count=0 */
   return ret;
 }
 
